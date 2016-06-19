5a45427cf747c19792387ca37ff618d9c4b58ca4
Lóna Lore <lonalore@freemail.hu>
2016-06-19 18:54:40 +0200

    Fixed XML.

--- languages/English/English_global.php
+++ languages/English/English_global.php
@@ -7,6 +7,6 @@
 
 define("LAN_PLUGIN_COOKIE_CONSENT_NAME", "Cookie Consent");
 define("LAN_PLUGIN_COOKIE_CONSENT_SUMMARY", "The most popular solution to the EU Cookie Law.");
-define("LAN_PLUGIN_COOKIE_CONSENT_DESCRIPTION", "The most popular solution to the EU Cookie Law.");
+define("LAN_PLUGIN_COOKIE_CONSENT_DESCRIPTION", "The most popular solution to the EU Cookie Law. Cookie Consent is a lightweight JavaScript plugin for alerting users about the use of cookies on your website.");
 
 define("LAN_PLUGIN_COOKIE_CONSENT_LIBRARY_01", "Silktide's 'cookieconsent2' library is missing, please download it from [x] and place it at [y].");
